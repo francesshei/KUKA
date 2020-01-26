d0b7756713bcca2011f19cf1f323aa489cd81299
andreafresco <andreafresco6@gmail.com>
2020-01-26 12:49:50 +0100

    Dynamical interaction

---
+++ Assets/KUKA_Collision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KUKA_Collision : MonoBehaviour
+{
+void OnCollisionEnter()
+    {
+     Debug.Log("We hit something!");
+
+    }
+}
